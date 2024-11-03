09e370478580f26de3d42cefab17c1623648945c
Vladislav_Morozov <vlad.2711moros2711@gmail.com>
2024-11-03 23:38:42 +0300

    Initialization

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions, Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
@@ -12,16 +12,18 @@ import time
 import pyautogui
 import os
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--incognito")
+
+# Инициализация драйвера с инкогнито-режимом
+driver = webdriver.Chrome(options=chrome_options)
 url = 'https://www.twitch.tv/'
 driver.get(url)
-
 
 def click_button(driver):
     button_xpath = '/html/body/div[1]/div/div[1]/nav/div/div[3]/div[3]/div/div[1]/div[1]/button'
     button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
     button.click()
-    time.sleep(2)  # Пауза, чтобы увидеть действие на экране
 
 
 num_tabs = int(input("Введите количество вкладок для открытия: "))
