99e5b1fd1bb03e4afbd0a04a8868a8d82c32f308
Vladislav_Morozov <vlad.2711moros2711@gmail.com>
2024-11-03 23:13:13 +0300

    Initialization

--- main.py
+++ main.py
@@ -17,14 +17,28 @@ url = 'https://www.twitch.tv/'
 driver.get(url)
 
 
-def click_button():
+def click_button(driver):
     button_xpath = '/html/body/div[1]/div/div[1]/nav/div/div[3]/div[3]/div/div[1]/div[1]/button'
     button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
     button.click()
-    time.sleep(2)
+    time.sleep(2)  # Пауза, чтобы увидеть действие на экране
 
 
-click_button()
+num_tabs = int(input("Введите количество вкладок для открытия: "))
 
+click_button(driver)
+
+for i in range(1, num_tabs):
+    # Открываем новую вкладку с тем же URL
+    driver.execute_script(f"window.open('{url}');")
+
+    # Получаем список вкладок и переключаемся на последнюю (только что открытую)
+    driver.switch_to.window(driver.window_handles[i])
+
+    # Даем время на загрузку страницы и выполняем нажатие кнопки
+    click_button(driver)
+
+# Возвращаемся на первую вкладку
+driver.switch_to.window(driver.window_handles[0])
 
 input()
